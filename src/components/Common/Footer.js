43db6be946c9d16cc96c830fa6ace3f9cd17bc49
opiyos <opiyosbenji@gmail.com>
2024-02-26 10:50:28 +0300

    created a commons folder in components and added header and footer

---
+++ src/components/Common/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<>
+			<h4>The Header</h4>
+		</>
+	);
+}
+
+export default Footer;
---
+++ src/components/Common/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+	return (
+		<>
+			<h4>The Header</h4>
+		</>
+	);
+}
+
+export default Header;
