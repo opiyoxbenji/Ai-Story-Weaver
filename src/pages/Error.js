1faba18b8ee78d94c11e04df96cd84d9ddff1733
opiyos <opiyosbenji@gmail.com>
2024-02-29 11:23:10 +0300

    minor updates

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../index.scss'; // Import your SCSS file to apply custom styles
 
 function Error() {
 	return (
 		<section className='section'>
-			{/* <h2>404</h2>
-      <p>page not found</p>
-      <Link to='/'>back home</Link> */}
-			<div class='d-flex align-items-center justify-content-center vh-100'>
-				<div className='text-center'>
+			<div className='d-flex align-items-center justify-content-center vh-100'>
+				<div className='error text-center'>
 					<h1 className='display-1 fw-bold'>404</h1>
 					<p className='fs-3'>
 						{' '}
-						<span className='text-danger'>Opps!</span> Page not found.
+						<span className='text-danger'>Oops!</span> Page not
+						found.
 					</p>
 					<p className='lead'>
 						The page you’re looking for doesn’t exist.
 					</p>
-					<a href='index.html' className='btn btn-warning'>
-						<Link to='/'>back home</Link>
-					</a>
+					{/* Apply custom styles for the link */}
+					<Link to='/' className='custom-link'>
+						Back Home
+					</Link>
 				</div>
 			</div>
 		</section>
