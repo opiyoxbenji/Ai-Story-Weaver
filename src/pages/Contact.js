a69301b0237a98427c9dceb9ba1652b4ad0923bf
opiyos <opiyosbenji@gmail.com>
2024-02-29 20:04:35 +0300

    added icons to the contacts page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,54 +1,165 @@
 import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../scss/app.scss';
+import { Container } from 'react-bootstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import {
+	faSquareXTwitter,
+	faSquareInstagram,
+	faSquareGithub,
+	faDiscord,
+} from '@fortawesome/free-brands-svg-icons';
 
 function Contact() {
 	return (
 		<>
-			<div className='container'>
-				<div className='form-container'>
-					<div className='left-container'>
-						<div className='left-inner-container'>
-							<h2>Let's Chat</h2>
-							<p>
-								Whether you have a question, want to start
-								writing, or simply want to connect.
-							</p>
-							<br />
-							<p>
-								Feel free to send me a message in the contact
-								form
-							</p>
-						</div>
-					</div>
-					<div className='right-container'>
-						<div className='right-inner-container'>
-							<form action='#'>
-								<h2 className='lg-view'>Contact</h2>
-								<h2 className='sm-view'>Let's Chat</h2>
-								<p>* Required</p>
-								<div className='social-container'>
-									<a href='#' className='social'>
-										<i className='fab fa-google-plus-g'></i>
-									</a>
-									<a href='#' className='social'>
-										<i className='fab fa-linkedin-in'></i>
-									</a>
-									<a href='#' className='social'>
-										<i className='fab fa-linkedin-in'></i>
-									</a>
-								</div>
-								<input type='text' placeholder='Name *' />
-								<input type='email' placeholder='Email *' />
-								<input type='tel' placeholder='Phone' />{' '}
-								<textarea
-									rows='4'
-									placeholder='Message'></textarea>
-								<button type='submit'>Submit</button>{' '}
-							</form>
+			<div class='container'>
+				<div class='text-center mt-5'>
+					<h1>Let's Support Each Other's Success</h1>
+				</div>
+
+				<div class='row'>
+					<div class='col-lg-7 mx-auto'>
+						<div class='card mt-2 mx-auto p-4 bg-light'>
+							<div class='card-body bg-light'>
+								<form id='contact-form' role='form'>
+									<div class='controls'>
+										<div class='row'>
+											<div class='col-md-6'>
+												<div class='form-group'>
+													<label for='form_name'>
+														Firstname
+													</label>
+													<input
+														id='form_name'
+														type='text'
+														name='name'
+														class='form-control'
+														placeholder='Enter your firstname'
+														required='required'
+														data-error='Firstname is required.'
+													/>
+												</div>
+											</div>
+											<div class='col-md-6'>
+												<div class='form-group'>
+													<label for='form_lastname'>
+														Lastname
+													</label>
+													<input
+														id='form_lastname'
+														type='text'
+														name='surname'
+														class='form-control'
+														placeholder='Enter your lastname'
+														required='required'
+														data-error='Lastname is required.'
+													/>
+												</div>
+											</div>
+										</div>
+										<div class='row'>
+											<div class='col-md-6'>
+												<div class='form-group'>
+													<label for='form_email'>
+														Email
+													</label>
+													<input
+														id='form_email'
+														type='email'
+														name='email'
+														class='form-control'
+														placeholder='Enter your email'
+														required='required'
+														data-error='Valid email is required.'
+													/>
+												</div>
+											</div>
+											<div class='col-md-6'>
+												<div class='form-group'>
+													<label for='form_need'>
+														Please specify your need
+													</label>
+													<select
+														id='form_need'
+														name='need'
+														class='form-control'
+														required='required'
+														data-error='Please specify your need.'>
+														<option
+															value=''
+															selected
+															disabled>
+															--Select Your
+															Issue--
+														</option>
+														<option>
+															Hallucination
+														</option>
+														<option>
+															Server slow down
+															issues
+														</option>
+														<option>
+															Crashing issues
+														</option>
+														<option>Other</option>
+													</select>
+												</div>
+											</div>
+										</div>
+										<div class='row'>
+											<div class='col-md-12'>
+												<div class='form-group'>
+													<label for='form_message'>
+														Message
+													</label>
+													<textarea
+														id='form_message'
+														name='message'
+														class='form-control'
+														placeholder='Write your message here.'
+														rows='4'
+														required='required'
+														data-error='Please, leave us a message.'></textarea>
+												</div>
+											</div>
+											<div class='col-md-12'>
+												<input
+													type='submit'
+													class='btn btn-success btn-send pt-2 btn-block'
+													value='Send Message'
+												/>
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
+			<Container>
+				<h3 className='socials'>Follow us on the various platforms</h3>
+				<div className='singleCol social-media-icons-white d-flex justify-content-evenly'>
+					<a href='https://twitter.com/'>
+						<FontAwesomeIcon icon={faSquareXTwitter} />
+					</a>
+					<a href='https://www.instagram.com/'>
+						<FontAwesomeIcon icon={faSquareInstagram} />
+					</a>
+					<a href='https://github.com/'>
+						<FontAwesomeIcon icon={faSquareGithub} />
+					</a>
+					<a href='https://discord.com/'>
+						<FontAwesomeIcon icon={faDiscord} />
+					</a>
+				</div>
+			</Container>
 		</>
 	);
 }
