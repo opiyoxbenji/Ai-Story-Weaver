cf3b64c01dbd027cbdc09ee4c7505df80f51cac9
opiyos <opiyosbenji@gmail.com>
2024-03-02 15:34:28 +0300

    fixing styling issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-	return (
+	return ( 
 		<>
 			<nav className='navbar navbar-expand-lg bg-dark navbar-dark'>
 				<div className='container'>
